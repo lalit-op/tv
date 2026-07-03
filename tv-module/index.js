ebb385cac3aa441f27be5b6a89f9ed78396446ad
lalit-op <lalitarana035@gmail.com>
2026-07-03 10:35:13 +0530

    Add files via upload

---
+++ tv-module/index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    name: "Live TV",
+
+    async onLoad() {
+        console.log("Live TV Module Loaded");
+    },
+
+    async onUnload() {
+        console.log("Live TV Module Unloaded");
+    }
+};
